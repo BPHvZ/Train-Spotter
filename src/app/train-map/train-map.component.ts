5cc98e6e5c60b4b0e0951549e7b2b81ed609f665
Bart van Zeist <bartvanzeist2000@gmail.com>
2020-05-24 14:51:22 +0200

    Initial train map component

---
+++ src/app/train-map/train-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-train-map',
+  templateUrl: './train-map.component.html',
+  styleUrls: ['./train-map.component.sass']
+})
+export class TrainMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

9eed9c2692c97e5c93e4860a9441aa40c206df8b
Bart van Zeist <bartvanzeist2000@gmail.com>
2020-06-18 16:40:34 +0200

    improve routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import {TrainMapComponent} from './components/train-map/train-map.component';
 import {AllStationsComponent} from './components/all-stations/all-stations.component';
 
 const routes: Routes = [
-  { path: 'kaart', component: TrainMapComponent, pathMatch: 'full' },
-  { path: 'stations', component: AllStationsComponent, pathMatch: 'full' },
-  { path: '',   redirectTo: '/stations', pathMatch: 'full' }
+  { path: 'kaart', component: TrainMapComponent },
+  { path: 'stations', component: AllStationsComponent},
+  { path: '',   redirectTo: '/kaart', pathMatch: 'full' }
 ];
 
 @NgModule({

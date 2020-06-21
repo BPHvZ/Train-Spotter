a4dc039b6f89c19d9eebb43f6250fcb5a4292916
Bart van Zeist <bartvanzeist2000@gmail.com>
2020-06-21 15:22:22 +0200

    test teamcity

--- karma.conf.js
+++ karma.conf.js
@@ -28,23 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['HeadlessChromium'],
-    customLaunchers: {
-      HeadlessChromium: {
-        base: 'ChromiumHeadless',
-        flags: [
-          '--no-sandbox',
-          '--remote-debugging-port=9222',
-          '--enable-logging',
-          '--user-data-dir=./karma-chrome',
-          '--v=1',
-          '--disable-background-timer-throttling',
-          '--disable-renderer-backgrounding',
-          '--proxy-bypass-list=*',
-          '--proxy-server=\'direct://\''
-        ]
-      }
-    },
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     restartOnFileChange: true
   });

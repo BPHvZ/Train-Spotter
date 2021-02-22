f8f4440627a90440e79485acdbc4bf3bd61163de
Bart van Zeist <bartvanzeist2000@gmail.com>
2021-02-22 22:41:14 +0100

    Make inputs passive

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,6 +61,7 @@ import "@angular/localize/init";
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import "./zone-flags";
 import "zone.js/dist/zone"; // Included with Angular CLI.
 
 /***************************************************************************************************

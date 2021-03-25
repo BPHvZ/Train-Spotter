583236cac39590197b06a829012cc8511cc3e23b
Bart van Zeist <bartvanzeist2000@gmail.com>
2021-03-25 19:12:12 +0100

    lint

--- src/app/directives/ng-init.directive.spec.ts
+++ src/app/directives/ng-init.directive.spec.ts
@@ -3,6 +3,7 @@ import { NgInitDirective } from "./ng-init.directive";
 describe("NgInitDirective", () => {
 	it("should create an instance", () => {
 		const directive = new NgInitDirective();
+
 		expect(directive).toBeTruthy();
 	});
 });

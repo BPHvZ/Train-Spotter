c4a5e3ed2ee150f26b2e5f29babfbc76d8e961b3
Bart van Zeist <bartvanzeist2000@gmail.com>
2021-03-26 16:06:52 +0100

    remove console.logs from worker

--- src/app/workers/jimp.worker.ts
+++ src/app/workers/jimp.worker.ts
@@ -32,8 +32,6 @@ import { jimpPrepareIcons } from "../helpers/jimp-helper";
  */
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 addEventListener("message", ({ data }: { data: Map<string, string> }) => {
-	console.log("WORKER");
-	console.log(data);
 	const iconURLs: Map<string, string> = data;
 	jimpPrepareIcons(iconURLs)
 		.pipe(take(1))

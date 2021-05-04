3601232eca91cd2cb4252e7ec63c8d4244b70d19
Bart van Zeist <bartvanzeist2000@gmail.com>
2021-05-04 16:18:15 +0200

    safari mapbox resize test 3

--- src/app/components/train-map/train-map/train-map.component.ts
+++ src/app/components/train-map/train-map/train-map.component.ts
@@ -193,12 +193,12 @@ export class TrainMapComponent implements OnInit, OnDestroy {
 				}
 			}
 			if (event instanceof ActivationEnd && Object.is(event?.snapshot?.component, TrainMapComponent)) {
-				// this.sharedDataService.trainMap.resize();
 				const trainMapElement = document.getElementById("mapbox-train-map");
 				const display = trainMapElement.style.display;
 				this.renderer.setStyle(trainMapElement, "display", "none");
 				console.log(trainMapElement.offsetHeight);
 				this.renderer.setStyle(trainMapElement, "display", display);
+				this.sharedDataService.trainMap.resize();
 			}
 		});
 		this.subscriptions.push(sub1);
